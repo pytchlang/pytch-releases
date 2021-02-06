ca26c7af8cdc82094b843f124bf612a6003e4288
Ben North <ben@redfrontdoor.org>
2021-02-06 11:09:18 +0000

    Prepare pytch-vm, pytch-build, pytch-webapp

--- develop.sh
+++ develop.sh
@@ -19,3 +19,46 @@ done
 )
 
 ./pytch-build/makesite/pytch-git-status.sh
+
+(
+    echo "Preparing VM ..."
+
+    cd pytch-vm
+    (
+        npm install
+        npm run devbuild
+        ( cd dist; ln -s skulpt.js skulpt.min.js )
+    ) > "$REPO_ROOT"/pytch-vm-preparation.out 2> "$REPO_ROOT"/pytch-vm-preparation.err
+
+    echo "Prepared VM"
+) &
+
+(
+    echo "Preparing build tools ..."
+
+    cd pytch-build
+
+    (
+        virtualenv -p python3 venv \
+            && source venv/bin/activate \
+            && pip install -r requirements_dev.txt \
+            && python setup.py install
+    ) > "$REPO_ROOT"/pytch-build-preparation.out 2> "$REPO_ROOT"/pytch-build-preparation.err
+
+    echo "Prepared build tools"
+) &
+
+(
+    echo "Preparing webapp ..."
+
+    cd pytch-webapp
+
+    (
+        npm install
+    ) > "$REPO_ROOT"/pytch-webapp-preparation.out 2> "$REPO_ROOT"/pytch-webapp-preparation.err
+
+    echo "Prepared webapp"
+) &
+
+wait
+echo "Built all; see *-preparation.(out|err) for details"
