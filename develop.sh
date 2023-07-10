9bbbf3c0604c9e1c924ce2c0171954fd588f7e84
Ben North <ben@redfrontdoor.org>
2023-07-10 13:53:36 +0100

    develop.sh: Update node version requirement to v18

--- develop.sh
+++ develop.sh
@@ -19,8 +19,8 @@ if [ "$have_all_tools" = "no" ]; then
 fi
 
 node_version=$(node --version)
-if [ "$(echo "$node_version" | grep -c -E '^v14[.]')" -ne 1 ]; then
-    echo Need node v14 but have "$node_version"
+if [ "$(echo "$node_version" | grep -c -E '^v18[.]')" -ne 1 ]; then
+    echo Need node v18 but have "$node_version"
     exit 1
 fi
 
